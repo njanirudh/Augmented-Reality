73ab07acce44bd990f98ac2876daebad0c21781e
njanirudh <anijaya9@gmail.com>
2018-07-23 13:08:34 +0530

    corrected aruco marker

--- singleton.py
+++ singleton.py
@@ -1,3 +1,7 @@
+
+# https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+
+# Singleton base class
 class Singleton(object):
     _instance = None
     def __new__(class_, *args, **kwargs):
