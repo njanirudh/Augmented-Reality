aae31e34e31805b9120903d18b3e4e88f822a618
njanirudh <anijaya9@gmail.com>
2018-07-23 20:39:06 +0530

    natural feature tracking marker

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from augmented_reality_service import AugmentedRealityService
 if __name__ == "__main__":
 
     ar_service = AugmentedRealityService()
-    ar_service.set_service_parameter_json("data/params.json")
+    ar_service.set_service_parameter_json("data/nft_params.json")
     ar_service.run_service()
 
--- natural_feature_marker.py
+++ natural_feature_marker.py
@@ -5,24 +5,73 @@ from marker_base import MarkerBase
 class NaturalFeatureMarker(MarkerBase):
 
     in_image = None
+    marker_image = None
+
+    json_params = None
+
+    cam_mat = None
+    dist_mat = None
+
+    r_vec = None
+    t_vec = None
+
+    feature_detector = None
+
+    marker_kp = None
+    marker_desc = None
+
+    input_kp = None
+    input_desc = None
 
     def __init__(self):
         pass
 
     def set_json_parameters(self, params):
-        pass
+
+        if(params["feature_type"] == "ORB"):
+            self.feature_detector = cv2.ORB_create()
+
+        if (params["feature_type"] == "FAST"):
+            self.feature_detector = cv2.FastFeatureDetector_create()
+
+        if (params["feature_type"] == "AKAZE"):
+            self.feature_detector = cv2.AKAZE_create()
+
+        self.set_marker_image(params["marker_path"])
+
 
     def set_calib_parameters(self,cam_mat,dist_mat):
-        pass
+        self.cam_mat = cam_mat
+        self.dist_mat = dist_mat
 
     def process_image(self):
-        pass
+
+        # BFMatcher with default params
+        bf = cv2.BFMatcher()
+        matches = bf.knnMatch(self.marker_desc, self.input_desc, k=2)
+        # Apply ratio test
+        good = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good.append([m])
+        # cv2.drawMatchesKnn expects list of lists as matches.
+        self.in_image = cv2.drawMatchesKnn(self.marker_image, self.marker_kp,
+                                            self.in_image , self.input_kp, good, self.in_image ,flags=2)
+
+    def set_marker_image(self,path):
+        self.marker_image = cv2.imread(path)
+        self.marker_kp , self.marker_desc = self.feature_detector.detectAndCompute(self.marker_image,None)
 
     def set_input_image(self, input):
-        self.in_image = cv2.cvtColor(input, cv2.COLOR_RGB2GRAY)
+        self.in_image = input
+        self.input_kp , self.input_desc = self.feature_detector.detectAndCompute(input,None)
 
     def get_output_image(self):
         return self.in_image
+
+    def get_pose(self):
+        return self.t_vec, self.r_vec
+
 
 if __name__ == "__main__":
     marker = NaturalFeatureMarker()
