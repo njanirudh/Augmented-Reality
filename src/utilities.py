3367f39abe30230628f45971b3964725c85bfafe
Anirudh <anijaya9@gmail.com>
2019-12-15 20:00:54 +0100

    Update utilities.py

--- src/utilities.py
+++ src/utilities.py
@@ -14,7 +14,7 @@ def convert_tx_rx_into_transformation(tx,rx):
     transformation_matrix = np.array([[r_matrix[0],r_matrix[3],r_matrix[6],0.0],
                                      [r_matrix[1],r_matrix[4],r_matrix[7],0.0],
                                      [r_matrix[2],r_matrix[5],r_matrix[8],0.0],
-                                     [-tx[0], -tx[1] , -tx[2],1.0]])
+                                     [tx[0], -tx[1] , -tx[2],1.0]])
     print(transformation_matrix)
     return transformation_matrix
 
